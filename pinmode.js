8c921663c82e2b23f003a98333dc9e4b1f6e8c35
root <root@beaglebone.(none)>
2014-09-03 23:33:35 +0000

    initial commit

---
+++ pinmode.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var b = require('bonescript');
+
+var button1 = 'P8_7';
+var button2 = 'P8_9';
+var button3 = 'P8_11';
+var button4 = 'P8_13';
+var button5 = 'P8_15';
+var button6 = 'P8_17';
+var button7 = 'P8_19';
+
+b.pinMode(button1, b.INPUT, 7, 'disabled');
+b.pinMode(button2, b.INPUT, 7, 'disabled');
+b.pinMode(button3, b.INPUT, 7, 'disabled');
+b.pinMode(button4, b.INPUT, 7, 'disabled');
+b.pinMode(button5, b.INPUT, 7, 'disabled');
+b.pinMode(button6, b.INPUT, 7, 'disabled');
+b.pinMode(button7, b.INPUT, 7, 'disabled');
+
